1497976af50f1685daca377780e15dd663a36314
Shiv Trivedi <shiv.v.trivedi@gmail.com>
2021-01-22 14:06:37 -0500

    memory optimization

--- main.py
+++ main.py
@@ -4,58 +4,108 @@ import glob
 from librosa_beat_track import beat_track
 import numpy as np
 from itertools import permutations
+import progressbar
 
-input_audio = "music/robbery.mp3"
+audio_file = "music/blueberry_faygo.mp3"
 # videos_dir = "clips/identical/"
-videos_dir = "output/test/"
+videos_dir = "csgo/"
 
-beats, onsets = beat_track(input_audio, plot=True)
-onsets = [float(i)/max(onsets) for i in onsets]
+beats, onsets = beat_track(audio_file, plot=False)
+onsets = [float(i) / max(onsets) for i in onsets]
 videos = []
 for x in glob.glob(videos_dir + "*"):
     videos.append(x.replace('\\', '/'))
 
 output_name = "output"
 cut_videos = videos
+
+# Do video analysis
 # cut_videos = []
-# for x in videos:
+# for x in progressbar.progressbar(videos):
 #     cut_videos += make_cuts(x, output_path, buff_size=40)
 # print(cut_videos)
+
+
+def permutation_generator(a):
+    a.sort()
+    yield list(a)
+    if len(a) <= 1:
+        return
+    first = 0
+    last = len(a)
+    while 1:
+        i = last - 1
+        while 1:
+            i = i - 1
+            if a[i] < a[i + 1]:
+                j = last - 1
+                while not (a[i] < a[j]):
+                    j = j - 1
+                a[i], a[j] = a[j], a[i]
+                r = a[i + 1:last]
+                r.reverse()
+                a[i + 1:last] = r
+                yield list(a)
+                break
+            if i == first:
+                a.reverse()
+                return
+
 
 count = 0
 clips = []
 beat_length = np.diff(np.array(beats)).mean()
 rounded_beats = []
 original_clips = []
+best_order = []
+max_weight = 0
 
 for clip in cut_videos:
     vfc = VideoFileClip(clip)
     rounded_beats.append(int(vfc.duration / beat_length))
     original_clips.append(vfc)
 
-perms = permutations(rounded_beats, len(rounded_beats))
+# TODO Check if audio file is longer than video clips
 
-weights = []
-orders = []
-for permutation in list(perms):
+print("Total Clips: ", len(original_clips))
+print("Calculating best order of clips")
+widgets = [
+    ' [', progressbar.Timer(), '] ',
+    progressbar.Bar()
+]
+
+bar = progressbar.ProgressBar(max_value=progressbar.UnknownLength, widgets=widgets)
+
+# Unoptimized original loop
+# for permutation in list(permutations(rounded_beats, len(rounded_beats))):
+
+for permutation in permutation_generator(rounded_beats):
     order = list(permutation)
     total_weight = 0
     running_beats = 0
     for element in order:
         running_beats += element
         total_weight += onsets[running_beats]
-    weights.append(total_weight)
-    orders.append(order)
+    if total_weight > max_weight:
+        max_weight = total_weight
+        best_order = order
+    bar.update()
 
-best_order = orders[weights.index(max(weights))]
-print(max(weights))
-print(best_order)
-for x in range(len(best_order)-1):
-    adjusted_clip = original_clips[rounded_beats.index(best_order[x])].fx(vfx.speedx, final_duration=best_order[x]*beat_length)
+print("\n")
+print("Original order: ", rounded_beats)
+print("Best order: ", best_order)
+for x in range(len(best_order) - 1):
+    adjusted_clip = original_clips[rounded_beats.index(best_order[x])].fx(vfx.speedx,
+                                                                          final_duration=best_order[x] * beat_length)
+    original_clips[rounded_beats.index(best_order[x])] = 0
+    rounded_beats[rounded_beats.index(best_order[x])] = 0
+    # original_clips.pop(rounded_beats.index(best_order[x]))
     clips.append(adjusted_clip)
 
+# Crossfade transitions
+# clips = [CompositeVideoClip([clip.fx(transfx.crossfadein, 0.5)]) for clip in clips]
 all_clips = concatenate_videoclips(clips)
-audio = AudioFileClip(input_audio)
+audio = AudioFileClip(audio_file)
 output_clip = all_clips.set_audio(audio.subclip(0, all_clips.duration))
 output_clip.write_videofile("output.mp4", fps=30)
 
